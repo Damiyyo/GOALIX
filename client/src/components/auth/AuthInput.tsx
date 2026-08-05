221f9b3c917b85f1de75b2d3e0878bd8004e3586
Oluwadamilola Banjo <banjooluwadamilola@yahoo.com>
2026-08-05 23:30:35 +0100

    password verify and visible

--- client/src/components/auth/AuthInput.tsx
+++ client/src/components/auth/AuthInput.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { useState } from "react";
+import { Eye, EyeOff } from "lucide-react";
 
 interface AuthInputProps {
   label: string;
@@ -20,34 +24,91 @@ const AuthInput = ({
   value,
   onChange,
 }: AuthInputProps) => {
+  const [showPassword, setShowPassword] =
+    useState(false);
+
+  const isPassword =
+    type === "password";
+
   return (
     <div className="space-y-2">
       <label
         className="
-          text-sm font-semibold
+          text-sm
+          font-semibold
           text-zinc-700
         "
       >
         {label}
       </label>
 
-      <input
-        type={type}
-        placeholder={placeholder}
-        name={name}
-        value={value}
-        onChange={onChange}
-        className="
-          h-14 w-full rounded-2xl
-          border border-zinc-300
-          bg-black px-5 outline-none
-          transition-all duration-300
+      <div className="relative">
+        <input
+          type={
+            isPassword
+              ? showPassword
+                ? "text"
+                : "password"
+              : type
+          }
+          placeholder={placeholder}
+          name={name}
+          value={value}
+          onChange={onChange}
+          className="
+            h-14
+            w-full
+            rounded-2xl
 
-          focus:border-zinc-700
-          focus:ring-4
-          focus:ring-zinc-200
-        "
-      />
+            border
+            border-zinc-300
+
+            bg-black
+
+            px-5
+            pr-14
+
+            outline-none
+
+            transition-all
+            duration-300
+
+            focus:border-zinc-700
+            focus:ring-4
+            focus:ring-zinc-200
+          "
+        />
+
+        {isPassword && (
+          <button
+            type="button"
+            onClick={() =>
+              setShowPassword(
+                !showPassword
+              )
+            }
+            className="
+              absolute
+              right-4
+              top-1/2
+
+              -translate-y-1/2
+
+              text-zinc-500
+
+              transition-colors
+
+              hover:text-white
+            "
+          >
+            {showPassword ? (
+              <EyeOff size={20} />
+            ) : (
+              <Eye size={20} />
+            )}
+          </button>
+        )}
+      </div>
     </div>
   );
 };
